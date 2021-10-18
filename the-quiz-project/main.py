12e3dee8385406732ee0ec25e93e97065cf5ca1f
behren <mobile.niclas@gmail.com>
2021-10-18 14:20:25 +0200

    replaced original question with questions fro open trivia db

--- the-quiz-project/main.py
+++ the-quiz-project/main.py
@@ -4,8 +4,8 @@ from data import question_data
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     question_bank.append(Question(question_text, question_answer))
 
 quiz = QuizBrain(question_bank)
