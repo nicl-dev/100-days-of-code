aa75e6c57ff22097ff6ef08d312f02424a7edd4b
behren <mobile.niclas@gmail.com>
2021-10-18 14:09:44 +0200

    added the quiz object and corresponding while loop to run the quiz.

--- the-quiz-project/main.py
+++ the-quiz-project/main.py
@@ -1,3 +1,4 @@
+from quiz_brain import QuizBrain
 from question_model import Question
 from data import question_data
 
@@ -6,3 +7,10 @@ for question in question_data:
     question_text = question["text"]
     question_answer = question["answer"]
     question_bank.append(Question(question_text, question_answer))
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz. \n"
+      f"Your final score was: {quiz.score}/{len(question_bank)}")
