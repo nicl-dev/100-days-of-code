4def7db09d494c4abdb6fca6fb5655913b954ac9
behren <mobile.niclas@gmail.com>
2021-10-18 14:38:46 +0200

    basic setup for the turtle-gui (day-18)

---
+++ turtle-gui/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("black", "green")
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
