613c4d23368b0cd37a07a46ed27d5807374b4532
behren <mobile.niclas@gmail.com>
2021-10-18 17:12:19 +0200

    255 colormode and modularized random_color

--- turtle-gui/main.py
+++ turtle-gui/main.py
@@ -1,10 +1,19 @@
 import turtle as t
 import random
 
+t.colormode(255)
 tim = t.Turtle()
 tim.shape("turtle")
 colors = ["dark orange", "deep sky blue", "indigo", "crimson"]
 directions = [0, 90, 180, 270]
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
 
 def draw_shape(num_sides):
@@ -19,8 +28,7 @@ def random_walk():
     tim.speed("fastest")
     tim.pensize(10)
     for _ in range(1, 500):
-        random_color = (random.random(), random.random(), random.random())
-        tim.color(random_color)
+        tim.color(random_color())
         direction = random.choice(directions)
         tim.setheading(direction)
         tim.forward(30)
