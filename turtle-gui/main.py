7a1d285734da07f9f09da208a378f6637da91164
behren <mobile.niclas@gmail.com>
2021-10-18 16:56:50 +0200

    added draw_shape and random_walk function

--- turtle-gui/main.py
+++ turtle-gui/main.py
@@ -1,20 +1,31 @@
-from turtle import Turtle, Screen
+import turtle as t
+import random
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("black", "green")
-
-
-
-
+tim = t.Turtle()
+tim.shape("turtle")
+colors = ["dark orange", "deep sky blue", "indigo", "crimson"]
+directions = [0, 90, 180, 270]
 
 
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+    num_sides += 1
 
 
+def random_walk():
+    tim.speed("fastest")
+    tim.pensize(10)
+    for _ in range(1, 500):
+        tim.color(random.choice(colors))
+        direction = random.choice(directions)
+        tim.setheading(direction)
+        tim.forward(30)
 
 
+random_walk()
 
-
-
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
