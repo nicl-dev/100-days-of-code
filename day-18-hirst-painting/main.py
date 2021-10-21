9a3beadf14e06d516f4c7e08100f6ce38b347f3c
behren <mobile.niclas@gmail.com>
2021-10-21 20:32:22 +0200

    extract 10 random colors from my image

--- day-18-hirst-painting/main.py
+++ day-18-hirst-painting/main.py
@@ -1,3 +1,13 @@
 import colorgram
 
-colors = colorgram.extract("image.jpg,")
+random_colors = colorgram.extract('image.jpg', 10)
+my_colors = []
+
+for color in random_colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    rgb = (r, g, b)
+    my_colors.append(rgb)
+
+print(my_colors)
