997b81a405ab4d895530845f490bc5877e0ce3dd
behren <mobile.niclas@gmail.com>
2021-10-21 21:38:29 +0200

    minor improvements to positioning and removed colorgram, because it's deprecated

--- day-18-hirst-painting/main.py
+++ day-18-hirst-painting/main.py
@@ -1,18 +1,6 @@
-import colorgram
 import turtle
 import random
 
-random_colors = colorgram.extract('image.jpg', 30)
-my_colors = []
-
-for color in random_colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    rgb = (r, g, b)
-    my_colors.append(rgb)
-
-# i manually deleted the whitish colors here
 filtered_colors = [(43, 104, 170), (233, 206, 115), (227, 152, 86), (186, 47, 75), (118, 88, 48), (230, 118, 145),
                    (110, 109, 188), (214, 61, 79), (54, 177, 113), (115, 185, 138), (122, 177, 213), (198, 18, 41),
                    (115, 170, 36), (33, 57, 113), (220, 53, 47), (26, 142, 109), (181, 168, 224), (155, 223, 193),
@@ -21,21 +9,21 @@ filtered_colors = [(43, 104, 170), (233, 206, 115), (227, 152, 86), (186, 47, 75
 
 tim = turtle.Turtle()
 tim.hideturtle()
+tim.penup()
 tim.speed("fast")
 turtle.colormode(255)
-y = 0
+x = -350
+y = -300
 
 
 def draw_dots(dot_count):
-    tim.penup()
-
     for _ in range(dot_count):
         tim.forward(60)
         tim.dot(30, random.choice(filtered_colors))
 
 
 for _ in range(10):
-    tim.goto(0, y)
+    tim.goto(x, y)
     draw_dots(10)
     y += 60
 
