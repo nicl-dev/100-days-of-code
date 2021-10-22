5cc6af896dc25e1b82dedb373479e40760168ea8
behren <mobile.niclas@gmail.com>
2021-10-22 22:24:14 +0200

    finished the race. first! ;)

--- day-19-turtle-race/main.py
+++ day-19-turtle-race/main.py
@@ -1,18 +1,35 @@
+import random
 from turtle import Turtle, Screen
 
-
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
-# user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+turtles = []
 y = 166
-x = -240
 
 for n in range(6):
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.color(colors[n])
-    tim.goto(x, y)
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[n])
+    new_turtle.goto(x=-240, y=y)
     y -= 66
+    turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+        if turtle.xcor() > 230:
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet.lower():
+                print(f"Congratulations! You won!! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+            is_race_on = False
 
 screen.exitonclick()
