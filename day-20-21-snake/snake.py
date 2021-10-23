bbf0ff7e5d0d09ced1956fa057fe1b1b2e1b8144
behren <mobile.niclas@gmail.com>
2021-10-23 10:59:17 +0200

    stop allow movement in opposite direction

--- day-20-21-snake/snake.py
+++ day-20-21-snake/snake.py
@@ -22,13 +22,17 @@ class Snake:
         self.snake_parts[0].forward(MOVEMENT_SPEED)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != 270:
+            self.head.setheading(90)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != 90:
+            self.head.setheading(270)
 
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != 0:
+            self.head.setheading(180)
 
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != 180:
+            self.head.setheading(0)
