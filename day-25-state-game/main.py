a0f6263e5d0c2e24fbc99fc9c1fe6a5e0f02433f
behren <mobile.niclas@gmail.com>
2021-11-20 00:30:42 +0100

    even more pandas experiments

--- day-25-state-game/main.py
+++ day-25-state-game/main.py
@@ -14,7 +14,22 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-average_temperature = data.temp.mean()
-max_temperature = data.temp.max()
-print(average_temperature)
-print(max_temperature)
+# average_temperature = data.temp.mean()
+# max_temperature = data.temp.max()
+# print(f"Average temperature: {average_temperature}")
+# print(f"Max temperature: {max_temperature}")
+# print(f"Max temperature day:\n{data[data.temp == max_temperature]}")
+
+# monday = data[data.day == "Monday"]
+# monday_temp_F = int(monday.temp * 9/5 + 32)
+# print(monday_temp_F)
+
+# Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+
+student_scores = pandas.DataFrame(data_dict)
+print(student_scores)
+student_scores.to_csv("student_scores.csv")
