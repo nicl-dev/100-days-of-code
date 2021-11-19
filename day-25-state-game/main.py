c7841da13f0f787b0560078637b255df337eb0ea
behren <mobile.niclas@gmail.com>
2021-11-19 23:48:27 +0100

    used pandas instead of csv

--- day-25-state-game/main.py
+++ day-25-state-game/main.py
@@ -1,12 +1,17 @@
 # with open("weather_data.csv") as the_weather:
 #     data = the_weather.readlines()
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     csv = csv.reader(data_file)
+#     temperatures = []
+#     for row in csv:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
 
-with open("weather_data.csv") as data_file:
-    csv = csv.reader(data_file)
-    temperatures = []
-    for row in csv:
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
