96317f778e007f65b563cb80eacbe63c0a59696c
behren <mobile.niclas@gmail.com>
2021-11-19 23:39:48 +0100

    completed csv module challenge

---
+++ day-25-state-game/main.py
@@ -0,0 +1,12 @@
+# with open("weather_data.csv") as the_weather:
+#     data = the_weather.readlines()
+
+import csv
+
+with open("weather_data.csv") as data_file:
+    csv = csv.reader(data_file)
+    temperatures = []
+    for row in csv:
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+    print(temperatures)
