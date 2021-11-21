3669ceae23f2604ea16077fbcb3e914f4c7704f5
behren <mobile.niclas@gmail.com>
2021-11-21 19:24:48 +0100

    finished first version  of the state game

---
+++ day-25-state-game/main.py
@@ -0,0 +1,30 @@
+import pandas
+import turtle
+
+screen = turtle.Screen()
+screen.setup(width=725, height=491)
+screen.title("U.S. States Game")
+background = "blank_states_img.gif"
+screen.addshape(background)
+turtle.shape(background)
+turtle.penup()
+turtle.tracer(0)
+df = pandas.read_csv("50_states.csv")
+game_is_on = "yes"
+score = 0
+all_states = df.state.to_list()
+correct_guesses = []
+
+while game_is_on:
+    answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state name?").title()
+    while answer_state in correct_guesses:
+        answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="You already guessed that "
+                                                                                   "state.\nWhat's another state "
+                                                                                   "name?").title()
+    if answer_state in all_states:
+        score += 1
+        correct_guesses.append(answer_state)
+        turtle.setpos(x=int(df[df.state == answer_state].x), y=int(df[df.state == answer_state].y))
+        turtle.write(answer_state)
+
+turtle.mainloop()
