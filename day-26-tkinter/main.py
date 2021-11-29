c7dbb3138a01a8906234956444f4746413b220fb
behren <mobile.niclas@gmail.com>
2021-11-29 12:26:55 +0100

    some more tkinter experiments

--- day-26-tkinter/main.py
+++ day-26-tkinter/main.py
@@ -6,6 +6,21 @@ window.minsize(width=500, height=300)
 
 # Label
 my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label.pack()
+my_label["text"] = "New Text"
+
+# Entry
+user_input = tkinter.Entry(width=10)
+user_input.pack()
+
+
+# Button
+def button_clicked():
+    new_text = user_input.get()
+    my_label.config(text=new_text)
+
+
+button = tkinter.Button(text="Click me", command=button_clicked)
+button.pack()
 
 window.mainloop()
