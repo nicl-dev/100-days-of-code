0c7d5cba41fb6bf445fa26f4394b65cc6d0d927a
behren <mobile.niclas@gmail.com>
2021-11-29 15:00:15 +0100

    first window, canvas and background setup

--- day-28-pomodoro-app/main.py
+++ day-28-pomodoro-app/main.py
@@ -1,4 +1,5 @@
-
+import tkinter
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +17,14 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=203, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.pack()
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+
+window.mainloop()
