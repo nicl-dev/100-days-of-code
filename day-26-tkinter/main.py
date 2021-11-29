ff7ca8056675f91b27af97d21a820db734fd0c28
behren <mobile.niclas@gmail.com>
2021-11-29 13:28:10 +0100

    fiddling around with tkinters grid

--- day-26-tkinter/main.py
+++ day-26-tkinter/main.py
@@ -6,12 +6,13 @@ window.minsize(width=500, height=300)
 
 # Label
 my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label.grid(row=0, column=0)
 my_label["text"] = "New Text"
 
 # Entry
-user_input = tkinter.Entry(width=10)
-user_input.pack()
+user_input = tkinter.Entry(width=15)
+user_input.insert(0, "Enter something.")
+user_input.grid(row=2, column=3)
 
 
 # Button
@@ -21,6 +22,9 @@ def button_clicked():
 
 
 button = tkinter.Button(text="Click me", command=button_clicked)
-button.pack()
+button.grid(row=1, column=1)
+
+new_button = tkinter.Button(text="I am a new button.")
+new_button.grid(row=0, column=2)
 
 window.mainloop()
