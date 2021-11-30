22f5a87bbb296f91bd9e47a04b76fb56fc62984e
behren <mobile.niclas@gmail.com>
2021-11-30 21:08:40 +0100

    completed the UI grid

--- day-29-password-manager/main.py
+++ day-29-password-manager/main.py
@@ -6,12 +6,36 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 ui = Tk()
 ui.title("Password Manager")
-ui.minsize(width=250, height=250)
+ui.minsize(width=450, height=250)
 ui.config(padx=20, pady=20)
 
 logo_canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="logo.png")
 logo_canvas.create_image(100, 100, image=logo)
-logo_canvas.grid(row=0, column=0)
+logo_canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+website_input = Entry(width=35)
+website_input.grid(row=1, column=1, columnspan=2, sticky="EW")
+
+username_label = Label(text="Username/Email:")
+username_label.grid(row=2, column=0)
+
+username_input = Entry(width=35)
+username_input.grid(row=2, column=1, columnspan=2, stick="EW")
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+password_input = Entry(width=21)
+password_input.grid(row=3, column=1, sticky="WE")
+
+generate_password_button = Button(text="Generate Password", width=14)
+generate_password_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
 ui.mainloop()
