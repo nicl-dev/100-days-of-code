33ed9077882e8f96aaeac2d74d9df1e14e0e850b
behren <mobile.niclas@gmail.com>
2021-11-30 20:36:54 +0100

    anchor is CENTER by default already

--- day-29-password-manager/main.py
+++ day-29-password-manager/main.py
@@ -11,7 +11,7 @@ ui.config(padx=20, pady=20)
 
 logo_canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="logo.png")
-logo_canvas.create_image(100, 100, image=logo, anchor=CENTER)
+logo_canvas.create_image(100, 100, image=logo)
 logo_canvas.grid(row=0, column=0)
 
 ui.mainloop()
