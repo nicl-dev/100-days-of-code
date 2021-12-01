a5532654ccc485ce867137f09a08b07b18304301
behren <mobile.niclas@gmail.com>
2021-12-01 18:35:20 +0100

    implemented password generation automatically adds generated password to the clipboard

--- day-29-password-manager/main.py
+++ day-29-password-manager/main.py
@@ -1,8 +1,28 @@
+import random
+import pyperclip
 from tkinter import *
 from tkinter import messagebox
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    password_list = []
+
+    [password_list.append(random.choice(letters)) for _ in range(random.randint(8, 10))]
+    [password_list.append(random.choice(symbols)) for _ in range(random.randint(2, 4))]
+    [password_list.append(random.choice(numbers)) for _ in range(random.randint(2, 4))]
+
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    password_input.delete(0, "end")
+    password_input.insert(END, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -56,7 +76,7 @@ password_input = Entry(width=21)
 password_input.grid(row=3, column=1, sticky="EW")
 
 # Buttons
-generate_password_button = Button(text="Generate Password", width=14)
+generate_password_button = Button(text="Generate Password", width=14, command=generate_password)
 generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
