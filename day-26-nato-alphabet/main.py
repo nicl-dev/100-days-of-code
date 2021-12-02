5c3ef40bab25669b609c24d6f09630f536da3f47
behren <mobile.niclas@gmail.com>
2021-12-02 19:30:58 +0100

    catch KeyError in case user doesn't enter letters

--- day-26-nato-alphabet/main.py
+++ day-26-nato-alphabet/main.py
@@ -21,11 +21,18 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-# Create a dictionary in this format:
+#  Create a dictionary in this format:
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
 # Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Please type a word.\n")
-phonetic = [nato_dict[letter] for letter in user_input.upper()]
-print(phonetic)
+is_running = True
+while is_running:
+    try:
+        user_input = input("Please type a word.\n")
+        phonetic = [nato_dict[letter] for letter in user_input.upper()]
+        is_running = False
+    except KeyError:
+        print("Sorry, only letter in the alphabet please.")
+    else:
+        print(phonetic)
