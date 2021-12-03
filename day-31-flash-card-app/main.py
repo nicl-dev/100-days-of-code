5b029bddd371085a2527dd5925eb9a3e207a5c6f
behren <mobile.niclas@gmail.com>
2021-12-04 00:28:15 +0100

    setup images

--- day-31-flash-card-app/main.py
+++ day-31-flash-card-app/main.py
@@ -1,2 +1,15 @@
-BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import  *
 
+root = Tk()
+canvas = Canvas()
+
+# Setup Images
+card_back = PhotoImage(file="images/card_back.png")
+card_front = PhotoImage(file="images/card_front.png")
+right = PhotoImage(file="images/right.png")
+wrong = PhotoImage(file="images/wrong.png")
+
+
+
+
+root.mainloop()
